fae985d282e1cb93093610e69b8cbd1b3565a82d
Loren Sheets <lorenmsheets@gmail.com>
2017-06-09 11:40:03 -0700

    copied flippening

--- web/views/layout.php
+++ web/views/layout.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml" xmlns:og="http://ogp.me/ns#">
 <head>
   {% include 'header.html' %}
 </head>
 
 <body>
-  {% include 'nav.html' %}
 
   {% block content %}{% endblock %}
+
 </body>
 </html>
