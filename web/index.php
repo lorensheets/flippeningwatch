3cb6dd2b212b233c0aadb79340b3db674eeb3040
Loren Sheets <lorenmsheets@gmail.com>
2017-06-09 20:06:21 -0700

    commits

--- web/index.php
+++ web/index.php
@@ -48,14 +48,14 @@ $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider
                    )
                )
 );
-$app->get('/db/', function(Request $request) use($app) {
+$app->get('/db/', function() use($app) {
   $in = $app['pdo']->prepare('CREATE TABLE IF NOT EXISTS test_table (
     id bigserial primary key,
     name int(11) NOT NULL
   )');
   $in->execute();
 
-  $value = $request->query->get('number');
+  $value = $app.request.parameters.get('number');
 
   $abc = $app['pdo']->prepare('INSERT INTO test_table (name) VALUES ( $value )');
   $abc->execute();
