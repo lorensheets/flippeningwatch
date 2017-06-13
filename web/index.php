747930fd634a35b42a70b6d8fb543406cabc944c
Loren Sheets <lorenmsheets@gmail.com>
2017-06-13 15:42:25 -0700

    new table

--- web/index.php
+++ web/index.php
@@ -60,7 +60,14 @@ $app->get('/', function() use($app) {
   array_push($api, $row['mkt_cap']);
   array_push($api, $row['btc_price']);
 
+  $uv = $app['pdo']->prepare('SELECT * FROM currencies');
+  $uv->execute();
 
+  while ($row = $uv->fetch(PDO::FETCH_ASSOC)) {
+    array_push($api, $row['currency']);
+    array_push($api, $row['price']);
+    array_push($api, $row['cap']);
+  }
 
   return $app['twig']->render('index.twig', array(
     'api' => $api
@@ -105,7 +112,8 @@ $app->get('/script1', function() use($app) {
   return $app['twig']->render('script1.html');
 });
 
-$app->get('/api/{mkt_cap}/{btc}/{eth}/{pct}/{btc_vol}/{eth_vol}/{pct_vol}/{btc_price}/{eth_price}/{btc_rwd}/{eth_rwd}/{pct_rwd}/{btc_tx}/{btc_nodes}', function($mkt_cap,$btc,$eth,$pct,$btc_vol,$eth_vol,$pct_vol,$btc_price,$eth_price,$btc_rwd,$eth_rwd,$pct_rwd,$btc_tx,$btc_nodes) use($app) {
+$app->get('/api/{mkt_cap}/{btc}/{eth}/{pct}/{btc_vol}/{eth_vol}/{pct_vol}/{btc_price}/{eth_price}/{btc_rwd}/{eth_rwd}/{pct_rwd}/{btc_tx}/{btc_nodes}',
+ function($mkt_cap,$btc,$eth,$pct,$btc_vol,$eth_vol,$pct_vol,$btc_price,$eth_price,$btc_rwd,$eth_rwd,$pct_rwd,$btc_tx,$btc_nodes) use($app) {
   $v = $app->escape($mkt_cap);
   $v1 = $app->escape($btc);
   $v2 = $app->escape($eth);
@@ -121,12 +129,50 @@ $app->get('/api/{mkt_cap}/{btc}/{eth}/{pct}/{btc_vol}/{eth_vol}/{pct_vol}/{btc_p
   $v12 = $app->escape($btc_nodes);
   $v13 = $app->escape($btc_price);
 
+  /* truncate tables */
   $truncate = $app['pdo']->prepare('TRUNCATE crypto');
   $truncate->execute();
 
+  /* insert data */
   $insert = $app['pdo']->prepare("INSERT INTO crypto (mkt_cap,btc,eth,pct,btc_vol,eth_vol,pct_vol,eth_price,btc_rwd,eth_rwd,pct_rwd,btc_tx,btc_nodes,btc_price) VALUES ( '$v','$v1','$v2','$v3','$v4','$v5','$v6','$v7','$v8','$v9','$v10','$v11','$v12','$v13' )");
   $insert->execute();
 
+  /* table 2 api */
+  /* json api for currencies from coinmarketcap */
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_URL, 'https://api.coinmarketcap.com/v1/ticker/');
+  $result = curl_exec($ch);
+  curl_close($ch);
+
+  $obj = json_decode($result);
+
+  $currencies = array();
+  $prices = array();
+  $caps = array();
+
+  /* truncate table */
+  $truncate_currencies = $app['pdo']->prepare('TRUNCATE currencies');
+  $truncate_currencies->execute();
+
+  for ($i = 0; $i < 11; $i++) {
+    $currency = $obj[$i]->name;
+    $currency_price = $obj[$i]->price_usd;
+    $currency_cap = $obj[$i]->market_cap_usd;
+    array_push($currencies, $currency);
+    array_push($prices, $currency_price);
+    array_push($caps, $currency_cap);
+  }
+
+  for ($i = 0; $i < 11; $i++) {
+    $table2_insert = $app['pdo']->prepare("INSERT INTO currencies (currency, price, cap) VALUES ( '$currencies[$i]','$prices[$i]','$caps[$i]' ) ");
+    $table2_insert->execute();
+  }
+
+
+
+  /* print results */
   return $v.'<br>'
   .$v1.'<br>'
   .$v2.'<br>'
@@ -140,9 +186,70 @@ $app->get('/api/{mkt_cap}/{btc}/{eth}/{pct}/{btc_vol}/{eth_vol}/{pct_vol}/{btc_p
   .$v10.'<br>'
   .$v11.'<br>'
   .$v12.'<br>'
-  .$v13;
+  .$v13.'<br>'
+  .$currencies[0]." ".$prices[0]." ".$caps[0]."<br>".
+  $currencies[1]." ".$prices[1]." ".$caps[1]."<br>".
+  $currencies[2]." ".$prices[2]." ".$caps[2]."<br>".
+  $currencies[3]." ".$prices[3]." ".$caps[3]."<br>".
+  $currencies[4]." ".$prices[4]." ".$caps[4]."<br>".
+  $currencies[5]." ".$prices[5]." ".$caps[5]."<br>".
+  $currencies[6]." ".$prices[6]." ".$caps[6]."<br>".
+  $currencies[7]." ".$prices[7]." ".$caps[7]."<br>".
+  $currencies[8]." ".$prices[8]." ".$caps[8]."<br>".
+  $currencies[9]." ".$prices[9]." ".$caps[9]."<br>".
+  $currencies[10]." ".$prices[10]." ".$caps[10];
 });
 
 
+$app->get('/currencies', function() use($app) {
+
+  /* json api for currencies from coinmarketcap */
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_URL, 'https://api.coinmarketcap.com/v1/ticker/');
+  $result = curl_exec($ch);
+  curl_close($ch);
+
+  $obj = json_decode($result);
+
+  $currencies = array();
+  $prices = array();
+  $caps = array();
+
+  /* truncate table */
+  $truncate_currencies = $app['pdo']->prepare('TRUNCATE currencies');
+  $truncate_currencies->execute();
+
+  for ($i = 0; $i < 11; $i++) {
+    $currency = $obj[$i]->name;
+    $currency_price = $obj[$i]->price_usd;
+    $currency_cap = $obj[$i]->market_cap_usd;
+    array_push($currencies, $currency);
+    array_push($prices, $currency_price);
+    array_push($caps, $currency_cap);
+  }
+
+  for ($i = 0; $i < 11; $i++) {
+    $table2_insert = $app['pdo']->prepare("INSERT INTO currencies (currency, price, cap) VALUES ( '$currencies[$i]','$prices[$i]','$caps[$i]' ) ");
+    $table2_insert->execute();
+  }
+
+  /* insert data */
+
+
+  return   $currencies[0]." ".$prices[0]." ".$caps[0]."<br>".
+  $currencies[1]." ".$prices[1]." ".$caps[1]."<br>".
+  $currencies[2]." ".$prices[2]." ".$caps[2]."<br>".
+  $currencies[3]." ".$prices[3]." ".$caps[3]."<br>".
+  $currencies[4]." ".$prices[4]." ".$caps[4]."<br>".
+  $currencies[5]." ".$prices[5]." ".$caps[5]."<br>".
+  $currencies[6]." ".$prices[6]." ".$caps[6]."<br>".
+  $currencies[7]." ".$prices[7]." ".$caps[7]."<br>".
+  $currencies[8]." ".$prices[8]." ".$caps[8]."<br>".
+  $currencies[9]." ".$prices[9]." ".$caps[9]."<br>".
+  $currencies[10]." ".$prices[10]." ".$caps[10];
+
+});
 
 $app->run();
