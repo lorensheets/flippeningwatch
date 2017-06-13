7358ae85ac410e0a33bcca866d8f1f12f8db0641
Loren Sheets <lorenmsheets@gmail.com>
2017-06-12 23:42:34 -0700

    day night toggle

---
+++ web/js/color_toggle.js
@@ -0,0 +1,63 @@
+var day = true;
+
+var scroll = function() {
+  var scrollTop = window.pageYOffset;
+  console.log(scrollTop);
+  if (scrollTop > 100) {
+    $('#toggle-color').fadeOut();
+  } else {
+    $('#toggle-color').fadeIn();
+  }
+}
+window.onscroll = scroll;
+
+
+$('#toggle-color').click(function() {
+  if (day == true) {
+
+    $(this).attr('src', 'images/moon.svg');
+
+    document.body.style.setProperty('--main-bg-color', '#131517');
+    document.body.style.setProperty('--main-text-color', '#eee');
+    document.body.style.setProperty('--table-bg', '#07090a');
+    document.body.style.setProperty('--ticker-color', '#ccc');
+    document.body.style.setProperty('--link-color', '#44f');
+    document.body.style.setProperty('--link-hover', '#fff');
+    document.body.style.setProperty('--light-grey', '#aaa');
+    document.body.style.setProperty('--border', '1px solid #444');
+    document.body.style.setProperty('--dark-border', '1px solid #333');
+
+    $('.invert').css({
+      'filter' : 'invert(100%)',
+      '-webkit-filter' : 'invert(100%)',
+      '-moz-filter' : 'invert(100%)',
+      '-ms-filter' : 'invert(100%)',
+      '-o-filter' : 'invert(100%)'
+    });
+
+    day = false;
+  } else {
+
+    $(this).attr('src', 'images/sun.png');
+
+    document.body.style.setProperty('--main-bg-color', '#fff');
+    document.body.style.setProperty('--main-text-color', '#222');
+    document.body.style.setProperty('--table-bg', '#f6f6f6');
+    document.body.style.setProperty('--ticker-color', '#444');
+    document.body.style.setProperty('--link-color', '#00f');
+    document.body.style.setProperty('--link-hover', '#222');
+    document.body.style.setProperty('--light-grey', '#888');
+    document.body.style.setProperty('--border', '1px solid #ddd');
+    document.body.style.setProperty('--dark-border', '1px solid #ccc');
+
+    $('.invert').css({
+      'filter' : 'invert(0%)',
+      '-webkit-filter' : 'invert(0%)',
+      '-moz-filter' : 'invert(0%)',
+      '-ms-filter' : 'invert(0%)',
+      '-o-filter' : 'invert(0%)'
+    });
+
+    day = true;
+  }
+});
