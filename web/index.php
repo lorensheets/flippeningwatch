3e8caf7d2d69fca9000930d4d27f9f9d05d23f52
Loren Sheets <lorenmsheets@gmail.com>
2017-06-21 00:01:37 -0800

    changes

--- web/index.php
+++ web/index.php
@@ -162,6 +162,9 @@ $app->get('/api/{mkt_cap}/{btc}/{eth}/{pct}/{btc_vol}/{eth_vol}/{pct_vol}/{btc_p
   $btc = (int)$obj[0]->market_cap_usd;
   $eth = (int)$obj[1]->market_cap_usd;
   $pct = ($eth/$btc)*100;
+  $btc_vol = (int)$obj[0]->24h_volume_usd;
+  $eth_vol = (int)$obj[1]->24h_volume_usd;
+  $pct_vol = ($eth_vol / $btc_vol) * 100;
 
   $currencies = array();
   $prices = array();
@@ -189,7 +192,10 @@ $app->get('/api/{mkt_cap}/{btc}/{eth}/{pct}/{btc_vol}/{eth_vol}/{pct_vol}/{btc_p
   return $mktcap.'<br>'
   .$btc.'<br>'
   .$eth.'<br>'
-  .$pct;
+  .$pct.'<br>'
+  .$btc_vol.'<br>'
+  .$eth_vol.'<br>'
+  .$pct_vol;
   /* print results */
   /*
   return $mktcap.'<br>'
