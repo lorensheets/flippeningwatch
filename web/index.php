5263034846a53be49b698f4654bece795bc14ccc
Loren Sheets <lorenmsheets@gmail.com>
2017-06-21 00:18:46 -0800

    changes

--- web/index.php
+++ web/index.php
@@ -163,7 +163,7 @@ $app->get('/api/{mkt_cap}/{btc}/{eth}/{pct}/{btc_vol}/{eth_vol}/{pct_vol}/{btc_p
   $btc1 = (int)$obj[0]->market_cap_usd;
   $eth1 = (int)$obj[1]->market_cap_usd;
   $pct1 = ($eth/$btc)*100;
-  $btc_vol1 = (int)$obj[0]["24h_volume_usd"];
+  $btc_vol1 = $obj[0]["24h_volume_usd"];
 
 
   $currencies = array();
