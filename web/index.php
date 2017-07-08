a73d63c3401b9961c51df513b693d28764314caa
Loren Sheets <lorenmsheets@gmail.com>
2017-07-08 14:06:21 -0700

    graph changes

--- web/index.php
+++ web/index.php
@@ -127,7 +127,7 @@ $app->get('/charts/', function() use($app) {
   $times = array();
   $interval = 0;
   foreach($values as $val){
-      if($val->x > 1356998400) {
+      if($val->x > 1438387200) {
         if($interval == 0) {
           array_push($dataset, $val->y);
           array_push($times, $val->x);
