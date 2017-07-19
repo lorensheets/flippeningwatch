a19d32d7716d1d0ef44050a44827b3e672e158a8
Loren Sheets <lorenmsheets@gmail.com>
2017-07-19 00:05:19 -0700

    changes

--- web/js/nav.js
+++ web/js/nav.js
@@ -1,25 +1,22 @@
 /* navigation indicator */
 
 // find out which page visitor is on
-
-if (window.location.href.includes("charts")) {
-
-  $('.navigation-bar a:nth-child(2)').css({
-    'border': '2px solid #dddddd',
-    'padding': '5px 5px 3px 5px',
-    'border-radius': '10px',
-    'background-color': '#f5f6f7',
-    'font-weight': '800'
-  });
-
-} else {
-
-  $('.navigation-bar a:first-child').css({
-    'border': '2px solid #dddddd',
-    'padding': '5px 5px 3px 5px',
-    'border-radius': '10px',
-    'background-color': '#f5f6f7',
-    'font-weight': '800'
-  });
-
-}
+$(document).ready(function() {
+  if (window.location.href.includes("charts")) {
+    $('.navigation-bar a:nth-child(2)').css({
+      'border': '2px solid #dddddd',
+      'padding': '5px 5px 3px 5px',
+      'border-radius': '10px',
+      'background-color': '#f5f6f7',
+      'font-weight': '800'
+    });
+  } else {
+    $('.navigation-bar a:first-child').css({
+      'border': '2px solid #dddddd',
+      'padding': '5px 5px 3px 5px',
+      'border-radius': '10px',
+      'background-color': '#f5f6f7',
+      'font-weight': '800'
+    });
+  }
+});
