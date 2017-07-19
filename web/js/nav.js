286611a45269fcb945cb3ef88d5502e83cc99ebb
Loren Sheets <lorenmsheets@gmail.com>
2017-07-19 00:39:06 -0700

    changes

--- web/js/nav.js
+++ web/js/nav.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   if (window.location.href.includes("charts")) {
     $('.navigation-bar a:nth-child(2)').css({
       'border': '2px solid #dddddd',
-      'padding': '5px 5px 3px 5px',
+      'padding': '4px 5px 3px 5px',
       'border-radius': '10px',
       'background-color': '#f5f6f7',
       'font-weight': '800'
