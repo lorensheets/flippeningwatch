75aa4262ec1ec8a830c366bc26d678e950718bd8
Loren Sheets <lorenmsheets@gmail.com>
2017-07-19 00:39:22 -0700

    changes

--- web/js/nav.js
+++ web/js/nav.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
   } else {
     $('.navigation-bar a:first-child').css({
       'border': '2px solid #dddddd',
-      'padding': '5px 5px 3px 5px',
+      'padding': '4px 5px 3px 5px',
       'border-radius': '10px',
       'background-color': '#f5f6f7',
       'font-weight': '800'
