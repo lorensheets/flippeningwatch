b1a67d9986857b5ec507f190a79bce68d152b064
Loren Sheets <lorenmsheets@gmail.com>
2017-07-19 00:03:27 -0700

    changes

--- web/js/nav.js
+++ web/js/nav.js
@@ -1,8 +1,25 @@
 /* navigation indicator */
-$('.navigation-bar a:first-child').css({
-  'border': '2px solid #dddddd',
-  'padding': '5px 5px 3px 5px',
-  'border-radius': '10px',
-  'background-color': '#f5f6f7',
-  'font-weight': '800'
-});
+
+// find out which page visitor is on
+
+if (window.location.href.includes("charts")) {
+
+  $('.navigation-bar a:nth-child(2)').css({
+    'border': '2px solid #dddddd',
+    'padding': '5px 5px 3px 5px',
+    'border-radius': '10px',
+    'background-color': '#f5f6f7',
+    'font-weight': '800'
+  });
+
+} else {
+
+  $('.navigation-bar a:first-child').css({
+    'border': '2px solid #dddddd',
+    'padding': '5px 5px 3px 5px',
+    'border-radius': '10px',
+    'background-color': '#f5f6f7',
+    'font-weight': '800'
+  });
+
+}
